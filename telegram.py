0248140319cb68149357764f8878bcf3d718f676
Alex Makarov <alexander.gmak@hotmail.com>
2024-03-23 04:34:12 +0100

    Finpussing

--- telegram.py
+++ telegram.py
@@ -3,14 +3,10 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.message import ContentType
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.dispatcher import FSMContext
-import logging
-import test
 from time import sleep
-#from get_client import get_tokens
 from tgtg import TgtgClient
 from time import gmtime, strftime
-import tgtg
-import asyncio
+import tgtg, asyncio, logging, test
 
 
 def get_tokens(emaila: str): 
@@ -57,38 +53,18 @@ def run():
 
         print("Feil ved innlogging: Kontroller at e-postadressen er korrekt.")
         return 
-
-
-# GETS CLIENT BEFORE ANYTHING STARTS. 
 client = get_tokens("")
 
 currentOut = []
 currentAvail = len(currentOut)
-
-avail = None 
-'''
-async def background_task():
-    while True:
-        await asyncio.sleep(10)  # Adjust the interval as needed
-        avail = get_available_items()
-        if not avail: 
-            currentOut = []
-            currentAvail = 0
-        #logging.info(avail)
-'''         
-#asyncio.create_task(background_task(client))
-
-
+avail = None     
 
 BOT_TOKEN = ''
-
 
 bot = Bot(BOT_TOKEN, parse_mode = "HTML", disable_web_page_preview = True)
 dp = Dispatcher(bot)
 
 user_state = dict()
-
-
 
 HELP_COMMAND = '''
 /start - Notification when bag available
@@ -125,79 +101,9 @@ async def help_command(message: types.Message):
 async def command_status(message: types.Message): 
     user_state[message.chat.id] = 'status'
     await message.reply(text=f"{currentAvail} available")
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# PASTE TELEGRAM BOT TOKEN FROM BOTFATHER 
-
-
-# Message.chat.id is my personal chat id. Commented it out since you'll also be using it. 
-
-'''
-
-'''
-'''
-@dp.message_handler(commands = 'email')
-async def worker(message: types.Message):
-
-    #if message.chat.id == 1778925351: 
-    user_state[message.chat.id] = 'email'
-    await message.answer (
-        text = 'Send your email please'
-    )    
-'''
-'''
-@dp.message_handler(content_types = ContentType.TEXT)
-async def message(message: types.Message):
-    # if message.chat.id == 1778925351: 
-    epost = message.text
-    match user_state[message.chat.id]:
-        
-        case "email": 
-            pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-
-            if re.match(pattern, message.text) is not None:
-                await message.answer (
-                    text = f'Verification passed'
-                )
-
-                client = get_tokens(message.text)
-                alleRegistrerte = []
-                while True: 
-                    sleep(10)
-                    svar = test.run(client, alleRegistrerte)
-                    if svar: await message.answer(text=svar)
-
-            else:
-                await message.answer (
-                    text = f'Verification failed'
-                )
-            user_state[message.chat.id] = None
-'''
-'''
-async def startup(dp):
-'''
-
 
 if __name__ == '__main__':
     executor.start_polling(dp)
-    #asyncio.run(run())
+ 
     
  
